4385a6f6739a835e8a701dd6ade8f4df78343e42
Jason Racey <jasonracey@gmail.com>
2022-03-19 19:30:44 -0700

    Fixes in progress on deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-dotnet build -c Release BandcampDownloader.sln
-rm -rf ../../Tools/BandcampDownloader
-mkdir ../../Tools/BandcampDownloader
-cp -r ./BandcampDownloaderConsole/bin/Release/net6.0/ ../../Tools/BandcampDownloader
+# /usr/local/share/dotnet/sdk/6.0.201/Microsoft.Common.CurrentVersion.targets(1220,5): error MSB3644: The reference 
+# assemblies for Xamarin.Mac,Version=v2.0 were not found. To resolve this, install the Developer Pack (SDK/Targeting 
+# Pack) for this framework version or retarget your application. You can download .NET Framework Developer Packs at 
+# https://aka.ms/msbuild/developerpacks 
+# [/Users/jasonracey/Files/Software/Projects/BandcampDownloader/BandcampDownloaderUI/BandcampDownloaderUI.csproj]
+#dotnet build -c Release BandcampDownloader.sln
 
+# deploy console
+echo "Deploying Console..."
+rm -Rf ../../Tools/BandcampDownloader
+mkdir ../../Tools/BandcampDownloader
+cp -R ./BandcampDownloaderConsole/bin/Release/netstandard2.1/ ../../Tools/BandcampDownloader
+
+# deploy app
+echo "Deploying UI..."
+rm -Rf /Applications/Bandcamp\ Downloader.app
+cp -Rf ./BandcampDownloaderUI/bin/Release/Bandcamp\ Downloader.app /Applications/Bandcamp\ Downloader.app
+
+echo "Done!"
